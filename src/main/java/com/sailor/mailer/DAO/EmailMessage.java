b9ce3a8f0277ead57a1df1a6f9331804146287c5
kamil <ka.motyka@o2.pl>
2020-04-01 12:31:10 +0200

    testing endpoint requested body

--- src/main/java/com/sailor/mailer/DAO/EmailMessage.java
+++ src/main/java/com/sailor/mailer/DAO/EmailMessage.java
@@ -1,6 +1,7 @@
 package com.sailor.mailer.DAO;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonRawValue;
 import lombok.AllArgsConstructor;
 import lombok.Value;
 
@@ -8,14 +9,19 @@ import lombok.Value;
 @Value
 public class EmailMessage {
 
+	@JsonRawValue
 	@JsonProperty("Subject")
 	private String subject;
+	@JsonRawValue
 	@JsonProperty("TextBody")
 	private String textBody;
+	@JsonRawValue
 	@JsonProperty("FromName")
 	private String fromName;
+	@JsonRawValue
 	@JsonProperty("FromAddress")
 	private String fromAddress;
+	@JsonRawValue
 	@JsonProperty("ToAddress")
 	private String toAddress;
 

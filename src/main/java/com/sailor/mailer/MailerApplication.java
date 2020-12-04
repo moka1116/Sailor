dcd805145724f1b3c8f5a47e01000ebf09b89e5c
TTIT\motykak <kamil.motyka@ttms.pl>
2020-12-04 16:34:57 +0100

    add login panel

--- src/main/java/com/sailor/mailer/MailerApplication.java
+++ src/main/java/com/sailor/mailer/MailerApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.client.RestTemplate;
 
 
@@ -19,6 +21,9 @@ public class MailerApplication {
 	RestTemplate restTemplate() {
 		return new RestTemplate();
 	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() { return new BCryptPasswordEncoder(); }
 }
 
 /*TODO
